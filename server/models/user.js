e363acdb2cce578e52795cca195de6fe453b6cb8
shaurya mittal <shauryamittal@192.168.1.7>
2026-06-24 21:38:22 +0530

    User model added

---
+++ server/models/user.js
@@ -0,0 +1,62 @@
+// imports the mongoose library
+const mongoose = require("mongoose");
+
+// refer class digram to know the details that need to be added in the user schema
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,  // marks the field as mandatory
+        trim : true     // Removes extra spaces from the beginning and end.
+    },
+    lastname:{
+       type:String,
+       required:true,  
+       trim : true,
+    },
+    email:{
+        type:String,
+        required:true,  
+        trim : true,
+    },
+    password:{
+        type:String,
+        required:true,  
+    },
+    accountType:{
+        type:String,
+        enum:["Admin","Student","Instructor"], // In Mongoose, enum is used to restrict a field to a fixed set of allowed values.
+        required : true,
+    },
+    additionalDetails:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"Profile"
+        // used to refer to the profile model
+    },
+    courses:[
+        // since there are multiple courses create a array
+        {
+          type:mongoose.Schema.Types.ObjectId,
+          ref:"Course",
+          // used to refer to the course module
+        }
+    ],
+    image:{
+        // add image of the user for profile
+        type:String,
+        required:true,
+        // type is string because a url will be added
+    },
+    courseProgress:[
+        // since there are multiple courses create a array
+        {
+          type:mongoose.Schema.Types.ObjectId,
+          ref:"CourseProgress",
+           // used to refer to the course progress module
+        }
+    ],
+});
+
+// used to export the User model so it can be used in other files.
+module.exports = mongoose.model("User",userSchema);
