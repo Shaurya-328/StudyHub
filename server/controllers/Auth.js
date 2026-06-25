9e468464e78c956b9d2e612806f0961394a3f84f
shaurya mittal <shauryamittal@192.168.1.5>
2026-06-25 20:16:32 +0530

    Added signup and login controllers

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -2,6 +2,9 @@
 const User = require("../models/User");
 const OTP = require("../models/OTP");
 const otpGenerator = require("otp-generator");
+const  bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 // sendOTP
 // our email sender required email and otp as input so this is where we generate otp
@@ -76,3 +79,200 @@ exports.sendOTP = async (req , res) =>{
         })
     }
 };
+
+
+// sign up
+exports.signUp = async (req,res) =>{
+     try {
+        
+    // step-1 fetch data from req body(detils entered by the user during sign up like name,age etc)
+    const {
+        firstName,
+        lastName,
+        email,
+        password,
+        confirmPassword,
+        accountType,
+        contactNumber,
+        otp
+    } = req.body
+
+    // step-2 validating the data
+
+    // check if some data is absent
+    if(!firstName || !lastName || !email || !password || !confirmPassword || !contactNumber || !otp){
+        // no need to add accounttype since it is a tab so it would automatically assign a value as user or instructor
+        return res.status(400).json({
+            success:false,
+            message:"All fields are required",
+        })
+    }
+
+    //check whether the pass and confirm pass are same 
+    if(password !== confirmPassword){
+        return res.status(400).json({
+            // 400 ---> invalid data 
+            success:false,
+            message:"Password and Confirm Password does not match, please Try again",
+        })
+    }
+
+    // check is user already present or not
+    const existingUser = await User.findOne({email});
+
+    // if present return (cannot signup)
+    if(existingUser){
+        return res.status(400).json({
+            // 400 ---> invalid data 
+            success:false,
+            message:"User is already registered",
+        })
+    }
+
+    // step-3 find most recent otp for the user 
+    // use the CreatedAt property of the otp to get most recent one
+    const recentOtp = await OTP.find({email}).sort({createdAt:-1}).limit(1); 
+    console.log(recentOtp);
+
+    // validate OTP
+    if(recentOtp.length==0){
+        // otp not found 
+        return res.status(400).json({
+            success:false,
+            message:"OTP not found",
+        })
+    }else if(otp !== recentOtp[0].otp){
+        // invalid otp entered by user (does not match from the otp stored in database)
+        return res.status(400).json({
+            success:false,
+            message:"Invalid OTP",
+        })
+    }
+
+    // once the otp is correct
+    // we need to create a entry of the user in the database
+
+    // hash password to store in database
+    // using npm bcrypt to hash password
+    const hashedPassword = await bcrypt.hash(password,10);
+    // 10 just indicates the number of rounds of hashing
+
+    // create profile document also because additionaldetails refer profile schema
+    const profileDetails = await Profile.create({
+        gender:null,
+        dateOfBirth:null,
+        about:null,
+        contactNumber:null,
+    });
+
+    const user = await User.create({
+        firstName,
+        lastName,
+        email,
+        contactNumber,
+        password:hashedPassword,
+        accountType,
+        additionalDetails:profileDetails._id,
+        // iamge uses a external api which genertes a image for the profile from the firstname and the lastname initials
+        image:`https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`,
+    });
+
+    // generate the response
+    return res.status(200).json({
+            success:true,
+            message:'User is registered Successfully',
+            user,
+        });
+     } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            // 500 ---> server error
+            success:false,
+            message:"User cannot be registrered. Please try again",
+        })
+     }
+}
+
+
+
+// login 
+exports.login = async (req,res) =>{
+    try {
+        // get data from req body
+        const {email, password} = req.body;
+
+        // validation data(check for unfilled fields)
+        if(!email || !password) {
+            return res.status(400). json({
+                success:false,
+                message:'All fields are required, please try again',
+            });
+        }
+
+        // check user already exist or not
+        const user = await User.findOne({email}).populate("additionalDetails");
+       // additionalDetails stores a Profile ObjectId.
+       // populate("additionalDetails") retrieves the corresponding
+       // Profile document and attaches it to the user object.
+
+        if(!user){
+            // user not found in database
+            return res.status(401).json({
+                success:false,
+                message:"User is not registered . Sign up"
+            })
+        }
+
+
+        // generate JWT after password matching (since password is hashed we need to use bcrypt)
+        // compare if the input password match with the one in the database
+            if(await bcrypt.compare(password, user.password)) {
+                // password matched
+            const payload = {
+                email: user.email,
+                id: user._id,
+                accountType:user.accountType,
+            }
+            const token = jwt.sign(payload, process.env.JWT_SECRET, {
+                // JWT_Secret is used so that no one can change the details of the jwt token issued by the server
+                expiresIn:"2h", // after that user needs to login again with password
+            });
+            // add the token to the user profile
+            user.token = token;
+
+            // remove password before sending user data to frontend
+            user.password= undefined;
+
+            //create cookie and send response
+            const options = {
+                // how the browser stores the cookie
+                expires: new Date(Date.now() + 3*24*60*60*1000),  // valid for 3 days
+                httpOnly:true,
+            }
+            // create a cookie named "token" that stores the JWT
+            res.cookie("token", token, options).status(200).json({
+                // send the cookie (the cookie stores the jwt token in the user browser)
+                success:true,
+                token,  // jwt token
+                user,   // user profile after removing the password
+                message:'Logged in successfully',
+            })
+
+        }
+        else {
+            // password did not match
+            return res.status(401).json({
+                success:false,
+                message:'Password is incorrect',
+            });
+        }  
+    }
+        catch (error) {
+         console.log(error);
+         return res.status(500).json({
+            success:false,
+            message:'Login Failure, please try again',
+        });
+    }
+}
+
