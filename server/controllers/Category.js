94bd7f972aa7d938063b6c319cc6d272e61f5831
shaurya mittal <shauryamittal@192.168.1.5>
2026-06-27 12:21:45 +0530

    updated course and category controller and added rating and review controller

--- server/controllers/Category.js
+++ server/controllers/Category.js
@@ -60,3 +60,84 @@ exports.showAllCategories = async(req,res) =>{
         })
     }
 }
+
+//categoryPageDetails 
+
+exports.categoryPageDetails = async (req, res) => {
+    try {
+
+          // fetch data
+          const { categoryId } = req.body
+
+          // Get courses for the specified category
+           const selectedCategory = await Category.findById(categoryId)
+                   // courses is referred in category schema so populate it
+                  .populate({
+                        path: "courses",
+                        match: { status: "Published" },
+                        populate: "ratingAndReviews",
+                    })
+                   .exec()
+  
+          // Handle the case when the category is not found(validation)
+           if (!selectedCategory) {
+             return res.status(404).json({ 
+                success: false,
+                message: "Category not found" })
+            }
+
+         // Handle the case when there are no courses
+           if (selectedCategory.courses.length === 0) {
+               return res.status(404).json({
+                   success: false,
+                   message: "No courses found for the selected category.",
+                })
+            }
+  
+         // Get courses for other categories(suggestions)
+          const categoriesExceptSelected = await Category.find({
+                 _id: { $ne: categoryId },
+            })
+
+           let differentCategory = await Category.findOne(
+                categoriesExceptSelected[getRandomInt(categoriesExceptSelected.length)]
+                 ._id
+              ) 
+              .populate({
+                   path: "courses",
+                   match: { status: "Published" },
+               })
+              .exec()
+
+      // Get top-selling courses across all categories
+      const allCategories = await Category.find()
+        .populate({
+          path: "courses",
+          match: { status: "Published" },
+          populate: {
+            path: "instructor",
+        },
+        })
+        .exec()
+      const allCourses = allCategories.flatMap((category) => category.courses)
+      const mostSellingCourses = allCourses
+        .sort((a, b) => b.sold - a.sold)
+        .slice(0, 10)
+
+
+      res.status(200).json({
+        success: true,
+        data: {
+          selectedCategory,
+          differentCategory,
+          mostSellingCourses,
+        },
+      })
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error",
+        error: error.message,
+      })
+    }
+  }
