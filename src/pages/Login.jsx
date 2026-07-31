52094050b299fd9c87b65a5e55067fd72a25cfad
shaurya mittal <shauryamittal@192.168.1.6>
2026-07-31 22:37:55 +0530

    Bugs-2

---
+++ src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import loginImg from "../assets/Images/login.webp"
+import Template from "../components/core/Auth/Template"
+
+function Login() {
+  return (
+    // create a component beacuse almost the same Ui is use for the signup and the login page
+    <Template
+      // passing the props
+      title="Welcome Back"
+      description1="Build skills for today, tomorrow, and beyond."
+      description2="Education to future-proof your career."
+      image={loginImg}
+      formType="login"
+    />
+  )
+}
+
+export default Login
